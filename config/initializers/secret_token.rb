574abaed22783608350270bf0ba31832b9499e53
Lucas Roxo Mundim <lucas.mundim@gmail.com>
2011-10-22 18:59:02 -0200

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BossaInRio::Application.config.secret_token = '2de2fc94fa24a69c4a80e44afbe99c4a950eef060e31643e2b227ca0c6b3b0a22a67a09fd2dbc9048a330b7d1dee4f3753377977ac5503abac5ad770f8b1bbb6'
